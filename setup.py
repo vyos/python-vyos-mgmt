3515194ab93359ff7bb8ccc295c840bf335ff185
Hochikong <michellehzg@gmail.com>
2016-05-21 15:58:38 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#author=hochikong
+
+from setuptools import setup, find_packages
+setup(
+    name="vyroute",
+    version="0.1",
+    packages=find_packages(),
+
+    description="A library for VyOS routing setting",
+    long_description="A library for VyOS routing setting",
+    author="hochikong",
+    author_email="michellehzg@gmail.com",
+
+    license="Apache License2.0",
+    platforms="Independant",
+)
