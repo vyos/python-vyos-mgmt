7e548971cdecc5eefc671658060f7e62d772ff29
Daniil Baturin <daniil@baturin.org>
2016-09-06 13:21:14 +0600

    Proper license headers and metadata.

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
-# Copyright (c) 2016 Hochikong
+# Copyright (c) 2016 VyOS maintainers and contributors
+# Portions copyright 2016 Hochikong
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 from setuptools import setup, find_packages
 setup(
@@ -7,12 +28,12 @@ setup(
     packages=find_packages(),
     install_requires=['pexpect'],
 
-    description="A library for VyOS configurations",
-    long_description="A library for VyOS configurations",
-    author="Hochikong",
-    author_email="hochikong@foxmail.com",
+    description="A library for configuring VyOS routers over SSH",
+    long_description="A library for configuring VyOS routers over SSH",
+    author="VyOS maintainers and contributors",
+    author_email="maintainers@vyos.net",
 
     license="MIT",
-    keywords="A library for VyOS configurations",
+    keywords="VyOS, EdgeOS, Vyatta",
     url="https://github.com/vyos/python-vyos-mgmt"
 )
